730206f3a7a376d09fbd88bd85a73c29c4cd376a
niya1123 <yoheikurioka@gmail.com>
2018-06-30 15:54:26 +0900

    詳細とonloadを追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['top', 'web', 'unity', 'java', 'ios', '3d'],
+    anchors: ['top', 'web', 'unity', 'java', 'ios', '3d','detail'],
   });
 });
